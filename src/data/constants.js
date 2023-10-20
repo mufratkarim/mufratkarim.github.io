e3237ff8dbe7d2b1f4aad49e1d0c436a034610d8
Mufrat Karim Aritra <mufratkarimaritra@gmail.com>
2023-10-20 10:51:53 -0400

    Refactored the hardcoded links into small chunks and moved them onto Constants file

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,8 +2,31 @@ const CONSTANTS = {
   moviemateTitle: "Movie Mate (Stream Trailers)",
   moviemateDes:
     "Movie Mate is a trailblazing cross-platform mobile application that redefines the movie-watching experience, serving over 850,000 movie and 2 million actor profiles without a single ad. Architected with cutting-edge algorithms, the app merges data from TMDB and OMDB APIs to deliver unparalleled access to actor social media accounts and IMDb ratings. Launched under agile methodologies, the app has rapidly achieved market presence on both Google Play Store and Apple App Store, testament to its innovative features and robust backend infrastructure",
+  moviemateiOS:
+    "https://apps.apple.com/us/app/movie-mate-stream-trailers/id6445921143",
+  moviemateAndroid:
+    "https://play.google.com/store/apps/details?id=com.mka.moviemate",
   newspaperDesc:
     "Three iOS apps that seamlessly blends traditional edition of physical newspapers (Wall Street Journal, New York Times) with interactive AR video experiences, leveraging Apple's state-of-the-art ARKit and AI frameworks.",
+  newspaperLink: "https://github.com/mufratkarim/Artificial-Newspapers",
+  wsjLink:
+    "https://github.com/mufratkarim/Artificial-Newspapers/tree/main/Wall%20Street%20Journal",
+  nytimesLink:
+    "https://github.com/mufratkarim/Artificial-Newspapers/tree/main/The%20New%20York%20Times",
+  dailyprophetLink:
+    "https://github.com/mufratkarim/Artificial-Newspapers/tree/main/Daily%20Prophet",
+  androidAppsLink: "https://github.com/stars/mufratkarim/lists/android-apps",
+  worldCupLink: "https://github.com/mufratkarim/World-Cup-2022-Tracker",
+  cleanCryptoLink: "https://github.com/mufratkarim/CleanCrypto",
+  nationalparkLink: "https://github.com/mufratkarim/National-Park-Service-Lite",
+  triviaLink: "https://github.com/mufratkarim/Trivia",
+  memorylockerLink: "https://github.com/mufratkarim/Memory-Locker",
+
+  iosAppsLink: "https://github.com/stars/mufratkarim/lists/ios-apps",
+  swiftstormLink: "https://github.com/mufratkarim/SwiftStorm",
+  secretmessengerLink: "https://github.com/mufratkarim/Secret-Messenger",
+  hackerNewsLogLink: "https://github.com/mufratkarim/Hacker-News-Log",
+
   androidAppsDesc: `
     <div style="font-size: medium;">
         <strong>World Cup 2022 Tracker:</strong> A comprehensive guide to the FIFA World Cup 2022, featuring real-time updates on matches, teams, and stats, built with modern Android libraries.<br><br>
@@ -27,12 +50,22 @@ const CONSTANTS = {
         <strong>RGB Colorgame Project:</strong> An advanced web development project that tests your RGB color recognizing skills by creating a logic puzzle where your task is to select the correct color based on the given RGB code.<br><br>
     </div>
   `,
+
+  citytutorsLink: "https://www.thecitytutors.org/",
+  sortingvisualizerLink: "https://mufratkarim.github.io/sorting-visualizer/",
+  rgbProjectLink: "https://mufratkarim.github.io/RGB-Color-Game-Project/",
   aboutDesc: `
-  Though my presidential oath in the picture is a bit childish, when it comes to app development, I'm as mature as they come. I've led my own mobile app teams to create gems like 'Movie Mate' which boasts over 850,000 movie and 2 million actor records. \n
+  Though my presidential oath in the picture is a bit childish, I'm as mature as they come, when it comes to app development. I've led my own mobile app teams to create gems like 'Movie Mate' which boasts over 850,000 movie and 2 million actor records. \n
   Innovatively, I've also blended print and digital through my artifical newspapers, turning static articles into dynamic videos in renowned publications like Wall Street Journal and the New York Times.\n
   Between coding and creativity, if there's an app challenge, it's either on my radar or it has been tackled, and don't just take my word for it. Check my resume, projects, etc. See for yourself! \n
     
     `,
+  playStoreProfile:
+    "https://play.google.com/store/apps/dev?id=6689304387412298609",
+  appleStoreProfile:
+    "https://apps.apple.com/us/developer/mufrat-aritra/id1674841006",
+  linkedin: "https://www.linkedin.com/in/mufrat-karim-aritra/",
+  github: "https://github.com/mufratkarim",
 };
 
 export { CONSTANTS };
--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ initSwiper();
 initPlyr();
 initTiltEffect();
 window.onload = function () {
+  document.getElementById("Linkedin").setAttribute("href", CONSTANTS.linkedin);
+  document.getElementById("Github").setAttribute("href", CONSTANTS.github);
+
   document.querySelector(".project-wrapper__text-title").textContent =
     CONSTANTS.moviemateTitle;
-
   document.querySelector(
     ".project-wrapper__text > .moviemateDesc > p"
   ).textContent = CONSTANTS.moviemateDes;
@@ -29,4 +31,67 @@ window.onload = function () {
     CONSTANTS.webDesc;
   document.querySelector(".about-wrapper__info-text").innerHTML =
     CONSTANTS.aboutDesc.split("\n").join("<br>");
+
+  document
+    .getElementById("playStoreProfileLink")
+    .setAttribute("href", CONSTANTS.playStoreProfile);
+  document
+    .getElementById("appleStoreProfileLink")
+    .setAttribute("href", CONSTANTS.appleStoreProfile);
+  document
+    .getElementById("moviemateiOSLink")
+    .setAttribute("href", CONSTANTS.moviemateiOS);
+  document
+    .getElementById("moviemateAndroidLink")
+    .setAttribute("href", CONSTANTS.moviemateAndroid);
+  document
+    .getElementById("newspaper")
+    .setAttribute("href", CONSTANTS.newspaperLink);
+  document.getElementById("wsj").setAttribute("href", CONSTANTS.wsjLink);
+  document
+    .getElementById("nytimes")
+    .setAttribute("href", CONSTANTS.nytimesLink);
+  document
+    .getElementById("dailyprophet")
+    .setAttribute("href", CONSTANTS.dailyprophetLink);
+
+  document
+    .getElementById("androidApps")
+    .setAttribute("href", CONSTANTS.androidAppsLink);
+  document
+    .getElementById("worldCup")
+    .setAttribute("href", CONSTANTS.worldCupLink);
+  document
+    .getElementById("cleanCrypto")
+    .setAttribute("href", CONSTANTS.cleanCryptoLink);
+  document
+    .getElementById("nationalpark")
+    .setAttribute("href", CONSTANTS.nationalparkLink);
+  document.getElementById("trivia").setAttribute("href", CONSTANTS.triviaLink);
+  document
+    .getElementById("memorylocker")
+    .setAttribute("href", CONSTANTS.memorylockerLink);
+
+  document
+    .getElementById("iosApps")
+    .setAttribute("href", CONSTANTS.iosAppsLink);
+  document
+    .getElementById("swiftstorm")
+    .setAttribute("href", CONSTANTS.swiftstormLink);
+  document
+    .getElementById("secretmessenger")
+    .setAttribute("href", CONSTANTS.secretmessengerLink);
+  document
+    .getElementById("hackerNewsLog")
+    .setAttribute("href", CONSTANTS.hackerNewsLogLink);
+
+  document
+    .getElementById("citytutors")
+    .setAttribute("href", CONSTANTS.citytutorsLink);
+  document
+    .getElementById("sortingvisualizer")
+    .setAttribute("href", CONSTANTS.sortingvisualizerLink);
+  document
+    .getElementById("rgbProject")
+    .setAttribute("href", CONSTANTS.rgbProjectLink);
 };
